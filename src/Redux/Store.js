88784028b0fa1897da5c2a37ffbaf32a3377381c
kallolmondal <kallolmondal003@gmail.com>
2024-08-10 15:59:43 +0530

    UpcomingMovies

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import bannerSlice from "./Banner/bannerSlice";
 import trendingMovieSlice from "./TrendingMovies/trendingMovieSlice";
 import nowPlayingMoviesSlice from "./NowPlayingMovies/nowPlayingMoviesSlice";
+import upcomingMoviesSlice from "./UpcomingMovies/upcomingMovieSlice";
+
 
 
 const store = configureStore({
@@ -9,6 +11,7 @@ const store = configureStore({
         bannerState: bannerSlice.reducer,
         trendingMovieState: trendingMovieSlice.reducer, 
         nowPlayingMovieState: nowPlayingMoviesSlice.reducer,
+        upcomingMoviesState: upcomingMoviesSlice.reducer,
     }
 })
 
