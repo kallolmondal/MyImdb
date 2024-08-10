8758556a0e9df03c6d764195f5c1f815c2226140
kallolmondal <kallolmondal003@gmail.com>
2024-08-10 13:13:46 +0530

    changes

--- src/Components/ShimmerBanner/shimmerBanner.jsx
+++ src/Components/ShimmerBanner/shimmerBanner.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { ShimmerThumbnail } from "react-shimmer-effects";
 
-function ShimmerBanner() {
-  return <ShimmerThumbnail height={550} rounded />;
+function ShimmerBanner({ height: height = 550, width: width = null }) {
+  return <ShimmerThumbnail height={height} width={width} rounded />;
 }
 
 export default ShimmerBanner;
